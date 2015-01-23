36b04f60d22eed459902884425894d6f1fe3fc4b
Pocket <p0ck31@gmail.com>
2015-01-23 15:40:06 -0800

    fix the adding to comment array finally

--- app.js
+++ app.js
@@ -2,26 +2,30 @@
   var app = angular.module('worth1000', []);
 
   app.controller('ContentController', function() {
-    this.stories = postedStories;
+
+    this.stories = [{
+      author: 'Gavriel Mayerhoff',
+      message: "This is my favorite photograph"
+    }, {
+      author: 'Aryeh Strum',
+      message: "I came down to this place thinking it would be the best..."
+    }, {
+      author: 'Alexa Petrelli',
+      message: "Since when did they meet?"
+    }, ];
 
     this.story = {};
 
     this.addComment = function(content) {
-      console.log("executing add comment");
-      $parent.stories.push(this.story);
+      console.log("executing add comment", content);
+      this.stories.push(content);
 
       this.story = {};
     };
   });
 
-  var postedStories = [{
-    author: 'Gavriel Mayerhoff',
-    message: "This is my favorite photograph"
-  }, {
-    author: 'Aryeh Strum',
-    message: "I came down to this place thinking it would be the best..."
-  }, {
-    author: 'Alexa Petrelli',
-    message: "Since when did they meet?"
-  }, ];
+
+
+
+
 })();
