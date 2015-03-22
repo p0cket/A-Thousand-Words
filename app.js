569adcb1966830cc72ea81b790cbcea77abbb947
Pocket <p0ck31@gmail.com>
2015-03-21 20:44:49 -0700

    Mid adding Firebase

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 (function() {
-  var app = angular.module('worth1000', []);
+  var app = angular.module('worth1000', ["firebase"]);
 
-  app.controller('ContentController', function() {
+  app.controller('ContentController', [ "$firebase",
+    function( $firebase) {
+
+    var ref = new Firebase("https://thousandwords.firebaseio.com/");
+
+    // create an AngularFire reference to the data
+    var sync = $firebase(ref);
+
+    // download the data into a local object
+    // $scope.data = sync.$asObject();
+
+    // synchronize the object with a three-way data binding
+    // click on `index.html` above to see it used in the DOM!
+    // syncObject.$bindTo($scope, "data");
+
+    this.stories = sync.$asArray();
 
     this.stories = [{
       author: 'Gavriel Mayerhoff',
@@ -18,14 +33,13 @@
 
     this.addComment = function(content) {
       console.log("executing add comment", content);
-      this.stories.push(content);
+      // this.stories.push(content);
+      this.stories.$add({content: content})
 
       this.story = {};
+
     };
-  });
-
-
-
-
+  }
+  ]);
 
 })();
