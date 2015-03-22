2b1d3a1f1d8e0d22d2dea929dd0cc6cdff93c4f8
Pocket <p0ck31@gmail.com>
2015-03-21 20:45:39 -0700

    Revert 36b04f6..569adcb
    This rolls back to commit 36b04f60d22eed459902884425894d6f1fe3fc4b.

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
 (function() {
-  var app = angular.module('worth1000', ["firebase"]);
+  var app = angular.module('worth1000', []);
 
-  app.controller('ContentController', [ "$firebase",
-    function( $firebase) {
-
-    var ref = new Firebase("https://thousandwords.firebaseio.com/");
-
-    // create an AngularFire reference to the data
-    var sync = $firebase(ref);
-
-    // download the data into a local object
-    // $scope.data = sync.$asObject();
-
-    // synchronize the object with a three-way data binding
-    // click on `index.html` above to see it used in the DOM!
-    // syncObject.$bindTo($scope, "data");
-
-    this.stories = sync.$asArray();
+  app.controller('ContentController', function() {
 
     this.stories = [{
       author: 'Gavriel Mayerhoff',
@@ -33,13 +18,14 @@
 
     this.addComment = function(content) {
       console.log("executing add comment", content);
-      // this.stories.push(content);
-      this.stories.$add({content: content})
+      this.stories.push(content);
 
       this.story = {};
-
     };
-  }
-  ]);
+  });
+
+
+
+
 
 })();
