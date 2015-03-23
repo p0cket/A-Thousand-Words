4594d2a3daf3c02a73aadcf6e402f3a28c79f506
Pocket <p0ck31@gmail.com>
2015-03-23 11:01:47 -0700

    FAIL

--- app.js
+++ app.js
@@ -2,30 +2,17 @@
   var app = angular.module('worth1000', []);
 
   app.controller('ContentController', function() {
-
-    this.stories = [{
-      author: 'Gavriel Mayerhoff',
-      message: "This is my favorite photograph"
-    }, {
-      author: 'Aryeh Strum',
-      message: "I came down to this place thinking it would be the best..."
-    }, {
-      author: 'Alexa Petrelli',
-      message: "Since when did they meet?"
-    }, ];
-
-    this.story = {};
-
-    this.addComment = function(content) {
-      console.log("executing add comment", content);
-      this.stories.push(content);
-
-      this.story = {};
-    };
+    this.stories = postedStories;
   });
 
-
-
-
-
+  var postedStories = [{
+    author: 'Gavriel Mayerhoff',
+    message: "This is my favorite photograph"
+  }, {
+    author: 'Aryeh Strum',
+    message: "I came down to this place thinking it would be the best..."
+  }, {
+    author: 'Alexa Petrelli',
+    message: "Since when did they meet?"
+  }, ];
 })();
